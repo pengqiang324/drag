bcdbf37f09b927575d059bada0bfb293f9ca401d
pengqiang324 <839556387@qq.com>
2018-03-17 13:53:55 +0800

    change show.js

--- show.js
+++ show.js
@@ -2,8 +2,6 @@ function show(id){
 	var oId = document.getElementById(id);
 	var oIndex = 0;
 	oId.onmousedown = function(){
-		alert(1)
-		console.log('我在学习git github');
-		document.write = "<p>你</p>";
+		alert(2222)
 	};
 }
